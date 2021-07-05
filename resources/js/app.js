7e4c79878e718fa1d9938674d939fc7f5808f157
Novansyah <rachadian.novansyah15@gmail.com>
2021-07-05 17:23:26 +0700

    learn vue router and shorthand binding

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
 require('./bootstrap');
+import Vue from 'vue';
+import router from "./route";
+import Index from "./Index";
+import VueRouter from 'vue-router';
+
+window.Vue = require('vue');
+Vue.use(VueRouter);
+
+const app = new Vue({
+    el: '#app',
+    router,
+    components: {
+        "index": Index,
+    }
+});
---
+++ resources/js/route.js
@@ -0,0 +1,23 @@
+import VueRouter from "vue-router";
+import ExampleComponent from "./components/ExampleComponent";
+import Example2 from "./components/ExampleComponent2.vue";
+
+const routes = [
+    {
+        path: "/",
+        component: ExampleComponent,
+        name: "home",
+    },
+    {
+        path: "/second",
+        component: Example2,
+        name: "second",
+    },
+];
+
+const router = new VueRouter({
+    routes: routes,
+    mode: "history",
+});
+
+export default router;
